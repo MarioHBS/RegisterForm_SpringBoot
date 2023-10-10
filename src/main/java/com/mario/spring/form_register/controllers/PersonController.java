3a6fc2f6cad943fac43ea449e4643b2cfb44f73c
MarioHBS <marioh90@gmail.com>
2023-10-10 10:44:01 -0300

    adicionada dependências do Thymeleaf no HTML; alteração das rotas no controller

--- src/main/java/com/mario/spring/form_register/controllers/PersonController.java
+++ src/main/java/com/mario/spring/form_register/controllers/PersonController.java
@@ -1,7 +1,9 @@
 package com.mario.spring.form_register.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PersonController {
@@ -11,8 +13,9 @@ public class PersonController {
     return "index";
   }
 
-  @GetMapping(path = "/second")
-  public String openSecond() {
+  @GetMapping(value = "/second")
+  public String openSecond(@RequestParam("p") String name, Model model) {
+    model.addAttribute("nome", name);
     return "second";
   }
 
