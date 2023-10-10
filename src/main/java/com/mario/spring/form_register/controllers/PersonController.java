d0044f69eaa667300178c4ccbb873b9707dfdcc3
MarioHBS <marioh90@gmail.com>
2023-10-10 09:28:27 -0300

    controller criado; página básica criada

---
+++ src/main/java/com/mario/spring/form_register/controllers/PersonController.java
@@ -0,0 +1,14 @@
+package com.mario.spring.form_register.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PersonController {
+
+  @GetMapping(path = "/")
+  public String openApp() {
+    return "index";
+  }
+
+}
