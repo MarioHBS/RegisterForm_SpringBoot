5b1725dcbd8c5ec1f0a67411e840ae34184076c9
Mário Henrique <marioh90@gmail.com>
2023-10-10 15:14:37 -0300

    adicionado html para listar pessoas; criado rota em controller

--- src/main/java/com/mario/spring/form_register/controllers/PersonController.java
+++ src/main/java/com/mario/spring/form_register/controllers/PersonController.java
@@ -1,9 +1,15 @@
 package com.mario.spring.form_register.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.mario.spring.form_register.pessoa.Person;
 
 @Controller
 public class PersonController {
@@ -19,9 +25,29 @@ public class PersonController {
 		return "second";
 	}
 
+	/**
+	 * @param model
+	 * @return
+	 */
+	@GetMapping(value = "/pessoa")
+	public String getAllPeople(Model model) {
+		List<Person> people = new ArrayList<Person>();
+		Person p = new Person("aacascac", "Manoel", "01/01/2000", "9898474754", "email@gmail.com", "65110000", "", 1,
+				"bairro", "São Luís", "MA");
+
+		people.add(p);
+		model.addAttribute("pessoas", people);
+		return "list_people";
+	}
+
 	@GetMapping(path = "/pessoa/cadastro")
 	public String addPerson() {
 		return "open_form";
+	}
+
+	@PostMapping(value = "pessoa/{object}")
+	public String savePerson() {
+		return "redirect:/open_form";
 	}
 	// https://www.thymeleaf.org/doc/tutorials/2.1/usingthymeleaf.html
 }
