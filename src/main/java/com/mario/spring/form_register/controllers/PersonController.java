0f94d5a2fa322a3f596160872dbafd2e60b1a3f2
MarioHBS <marioh90@gmail.com>
2023-10-10 11:40:08 -0300

    formatação de arquivos

--- src/main/java/com/mario/spring/form_register/controllers/PersonController.java
+++ src/main/java/com/mario/spring/form_register/controllers/PersonController.java
@@ -8,20 +8,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class PersonController {
 
-  @GetMapping(path = "/")
-  public String openApp() {
-    return "index";
-  }
+	@GetMapping(path = "/")
+	public String openApp() {
+		return "index";
+	}
 
-  @GetMapping(value = "/second")
-  public String openSecond(@RequestParam("p") String name, Model model) {
-    model.addAttribute("nome", name);
-    return "second";
-  }
+	@GetMapping(value = "/second")
+	public String openSecond(@RequestParam("p") String name, Model model) {
+		model.addAttribute("nome", name);
+		return "second";
+	}
 
-  @GetMapping(path = "/pessoa/cadastro")
-  public String addPerson() {
-    return "open_form";
-  }
-
+	@GetMapping(path = "/pessoa/cadastro")
+	public String addPerson() {
+		return "open_form";
+	}
+	// https://www.thymeleaf.org/doc/tutorials/2.1/usingthymeleaf.html
 }
