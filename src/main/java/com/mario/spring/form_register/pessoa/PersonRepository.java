98ea49d4351783b41446746857711eca2ba5d02c
MarioHBS <marioh90@gmail.com>
2023-10-10 10:21:48 -0300

    criado repositório JPA

---
+++ src/main/java/com/mario/spring/form_register/pessoa/PersonRepository.java
@@ -0,0 +1,5 @@
+package com.mario.spring.form_register.pessoa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, String> { }
