c2142310354028f36a8cb5b5ca827dd239cbcc2f
MarioHBS <marioh90@gmail.com>
2023-10-10 10:16:00 -0300

    finalizada contrução de Person

--- src/main/java/com/mario/spring/form_register/pessoa/Person.java
+++ src/main/java/com/mario/spring/form_register/pessoa/Person.java
@@ -1,5 +1,7 @@
 package com.mario.spring.form_register.pessoa;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -13,10 +15,32 @@ import lombok.ToString;
 @Setter
 @AllArgsConstructor
 @ToString
+@Entity
 @Table(name = "pessoa")
 public class Person {
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private String id;
 
+  @Column(name = "nome_completo", nullable = false)
+  private String name;
+  @Column(name = "nascimento", nullable = false)
+  private String birth;
+  @Column(name = "telefone", nullable = true)
+  private String number;
+  @Column(name = "e_mail", nullable = false)
+  private String email;
+
+  @Column(name = "cep")
+  private String adr_cep;
+  @Column(name = "logradouro")
+  private String adr_location;
+  @Column(name = "numero")
+  private int adr_numb;
+  @Column(name = "bairro")
+  private String adr_neighbor;
+  @Column(name = "cidade")
+  private String adr_city;
+  @Column(name = "estado")
+  private String adr_state;
 }
