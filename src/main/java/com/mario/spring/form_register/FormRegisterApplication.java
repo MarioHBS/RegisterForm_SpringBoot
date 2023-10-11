98c7288290745ed3dedeb7c586954f3315321aec
Mário Henrique <marioh90@gmail.com>
2023-10-11 10:31:01 -0300

    application.properties: atualizado com variáveis de ambiente

--- src/main/java/com/mario/spring/form_register/FormRegisterApplication.java
+++ src/main/java/com/mario/spring/form_register/FormRegisterApplication.java
@@ -3,10 +3,14 @@ package com.mario.spring.form_register;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class FormRegisterApplication {
 
 	public static void main(String[] args) {
+//		var env = System.getenv();
+//		System.out.println("user_db = " + env.get("MYSQL_DB_USER"));
 		SpringApplication.run(FormRegisterApplication.class, args);
 	}
 
