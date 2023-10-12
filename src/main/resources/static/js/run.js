0d0266daa1881ce32fac5dc16e91350cf270af75
Mário Henrique <marioh90@gmail.com>
2023-10-12 09:25:32 -0300

    máscara de telefone funcional

--- src/main/resources/static/js/run.js
+++ src/main/resources/static/js/run.js
@@ -23,5 +23,29 @@ function formatar(mascara, documento, event) {
 };
 
 function telephone(doc, event) {
-    // v.replace(/(\d{2})(\d{5})(\d{4})/, '($1) $2-$3')  
+    const pure = doc.value.replace(/\D/g, '');
+    const sz = pure.length;
+
+    if (sz > 11) {
+        event.returnValue = false
+        return;
+    };
+
+    var regex = /^[a-z]/i;
+    if (!regex.test(event.key)) {
+        // console.log("value: " + doc.value);
+        // console.log("pure: " + pure);
+        console.log("sz: " + sz);
+
+        if (sz === 0) doc.value += '(';
+        if (sz >= 2) {
+            doc.value = pure.replace(/^(\d{2})$/, '($1)');
+        }
+        if (sz === 3) {
+            doc.value = pure.replace(/^(\d{2})(\d+)$/, '($1) $2');
+        }
+        if (sz >= 4) doc.value = pure.replace(/^(\d{2})(\d{1})(\d+)$/, '($1) $2 $3');
+        if (sz > 7) doc.value = pure.replace(/^(\d{2})(\d{1})(\d{4})(\d+)$/, '($1) $2 $3-$4');
+    }
+    // return "";
 };
